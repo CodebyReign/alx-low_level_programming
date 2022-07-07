4ead3a737bf328565aa1b6ac3567f8f6d077be5d
CodebyReign <jokeawominure@rocketmail.com>
2022-07-06 17:48:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - entry point
+ * Description: print _putchar
+ * Return: 0
  */
 int main(void)
 {
-char h[] = "Main";
-int m;
-m = 0;
-while (m <= 8)
-{
-_putchar(h[m]);
-m++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }

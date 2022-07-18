8666c102d4381466355a5996f60277d0b1a66204
CodebyReign <jokeawominure@rocketmail.com>
2022-07-18 16:22:30 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,18 +7,18 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-int i = 0;
-int j;
-while (s[i] != '\0')
+int i, j;
+i = 0;
+while (s[i])
 {
 j = 0;
+while (accept[j])
 {
 if (s[i] == accept[j])
-{
 return (s + i);
-}
 j++;
 }
 i++;
+}
 return (0);
 }

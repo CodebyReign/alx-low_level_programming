cfd0c302e2b64bbfe37e4711e1c4bd91ba861418
CodebyReign <jokeawominure@rocketmail.com>
2022-07-19 15:22:45 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,26 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string using rot13
- * @str: the string to encode
- * Return: encode string
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ * Return: address of s
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
 int i, j;
-char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
-i = 0;
-while (str[i] != '\0')
+char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+for (i = 0; *(s + i); i++)
 {
-j = 0;
-while (alpha[j] != '\0')
+for (j = 0; j < 52; j++)
 {
-if (str[i] == alpha[j])
+if (a[j] == *(s + i))
 {
-str[i] = rot[j];
+*(s + i) = b[j];
 break;
 }
-j++;
 }
-i++;
 }
-return (str);
+return (s);
 }

2d37e7e3aeeb5be99436d73b89519e7a1fc8cd27
CodebyReign <jokeawominure@rocketmail.com>
2022-07-22 15:00:32 -0700

    6-abs.c

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * describes file for static library
+ */
+
+int main(void)
+{
+_puts
+("\"At the end of the day, my goal was to be the best hacker\"
+\n\t- Kevin Mitnick");
+return (0);
+}

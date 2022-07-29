426a41edf5519acc221dbfef760b26105ef0b9d8
CodebyReign <jokeawominure@rocketmail.com>
2022-07-29 13:02:17 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,78 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _strlen - give the length of a string
- * @s: the string
- * Return: the length of a string
- */
-int _strlen(char *s)
-{
-int i;
-for (i = 0 ; s[i] != '\0' ; i++)
-return (i);
-}
-
-/**
- * _strcat - concatenates two strings
- * @dest: input parameter string
- * @src: input parameter string
- * Return: dest
- */
-char *_strcat(char *dest, char *src)
-{
-int a;
-int b;
-a = 0;
-while (dest[a] != 0)
-{
-a++;
-}
-b = 0;
-while (src[b] != 0)
-{
-dest[a] = src[b];
-a++;
-b++;
-}
-return (dest);
-}
-
-/**
- * str_concat - concatenate to strings
- * @s1: the string to print
- * @s2: the string to print
- * Return: pointer that contains the content of s1 followed by s2
+ * str_concat - concatenate strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: NULL if failure
  */
 char *str_concat(char *s1, char *s2)
 {
-int length;
-char *space;
-if (s1 == '\0')
-{
+char *s;
+int a, b, c, d;
+if (s1 == 0)
 s1 = "";
-}
-if (s2 == '\0')
-{
+if (s2 == 0)
 s2 = "";
-}
-length = _strlen(s1) + _strlen(s2);
-if (s1 != '\0' && s2 != '\0')
+for (a = 0; s1[a] != '\0'; a++)
+;
+for (b = 0; s2[b] != '\0'; b++)
+;
+b++;
+s = malloc(a * sizeof(*s1) + b * sizeof(*s2));
+if (s == 0)
+return (NULL);
+for (c = 0, d = 0; c < a + b; c++)
 {
-space = malloc(sizeof(char) * length + 1);
-if (space == '\0')
-{
-return ('\0');
-}
+if (c < a)
+s[c] = s1[c];
 else
-{
-space = _strcat(space, s1);
-space = _strcat(space, s2);
+s[c] = s2[d++];
 }
-else
-{
-space = "";
-}
-return (space);
-free(space);
+return (s);
 }

5030840dd043ce668a4545f4098249ae58aba0af
CodebyReign <jokeawominure@rocketmail.com>
2022-07-29 14:02:02 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,35 +1,47 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * str_concat - concatenate strings
- * @s1: string 1
- * @s2: string 2
- * Return: NULL if failure
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
  */
-char *str_concat(char *s1, char *s2)
+
+int _strlen(char *s)
 {
-char *s;
-int a, b, c, d;
-if (s1 == 0)
-s1 = "";
-if (s2 == 0)
-s2 = "";
-for (a = 0; s1[a] != '\0'; a++)
-;
-for (b = 0; s2[b] != '\0'; b++)
-;
-b++;
-s = malloc(sizeof(*s1) *a) + (sizeof(*s2) *b);
-if (s == 0)
-return (NULL);
-for (c = 0, d = 0; c < a + b; c++)
+int l = 0;
+while (*s != '\0')
 {
-if (c < a)
-s[c] = s1[c];
-else
-s[c] = s2[d++];
+s++;
+l++;
 }
-return (s);
+return (l);
+}
+
+/**
+ * str_concat - Concat 2 strings.
+ * @s1: string
+ * @s2: string
+ * Return: char
+ */
+
+char *str_concat(char *s1, char *s2)
+{unsigned int l1, l2;
+char *conc, *tmp;
+if (!s1)
+s1 = "";
+else
+l1 = _strlen(s1);
+if (!s2)
+s2 = "";
+else
+l2 = _strlen(s2);
+conc = malloc(l1 + l2 + 1);
+if (!conc)
+return (0);
+tmp = conc;
+while (*s1)
+*tmp++ = *s1++;
+while ((*tmp++ = *s2++))
+;
+return (conc);
 }

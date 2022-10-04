4f9972d2e8a8e6e48665c5ba8c77fefea14a22ab
CodebyReign <jokeawominure@rocketmail.com>
2022-10-04 07:26:19 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcpy - copies the string
+ * @dest: 1st argument char type pointer
+ * @src:the pointer that receives the string
+ * Description: copies the string to an array
+ * Return: na
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < src[i]; i++)
+dest[i] = '\0';
+return (dest);
+}
